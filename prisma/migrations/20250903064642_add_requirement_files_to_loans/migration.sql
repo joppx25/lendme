b739fe3567699bc4f7d70283e22b8ee1ebf87bb6
Joefrey Ramos <joefreyramos25@gmail.com>
2025-09-05 14:14:03 +0800

    switch to postgresql

--- prisma/migrations/20250903064642_add_requirement_files_to_loans/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `loans` ADD COLUMN `requirement_files` JSON NULL;
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,12 @@
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "../generated/prisma";
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
 
-const adapter = new PrismaMariaDb({
-    host: process.env.DATABASE_HOST,
-    port: parseInt(process.env.DATABASE_PORT || '3306'),
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    connectionLimit: 5,
-})
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = new PrismaClient({
-  adapter
-});
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query', 'error', 'warn'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
